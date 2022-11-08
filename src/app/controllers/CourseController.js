217789a488773a64c3b4a028f8f901b56a06dffa
NH1321 <buithucnam02022002@gmail.com>
2022-11-08 23:08:56 +0700

    commit

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -4,11 +4,11 @@ class CourseController {
 
     // [GET] /courses/:slug
     show(req, res, next) {
-        Course.findOne({slug: req.params.slug}).lean()
-        .then(course => res.render('courses/show', {
-            course: course
-        }))
-        .catch(next);
+        Course.findOne({ slug: req.params.slug }).lean()
+            .then(course => res.render('courses/show', {
+                course: course
+            }))
+            .catch(next);
     }
 
     // [GET] /courses/create
@@ -18,22 +18,36 @@ class CourseController {
 
     // [POST] /courses/store
     store(req, res, next) {
-        const formData = {...req.body};
+        const formData = { ...req.body };
         formData.image = `https://img.youtube.com/vi/${req.body.videoID}/sddefault.jpg`;
         const course = new Course(formData);
         course.save()
-        .then(() => res.redirect('/'))
-        .catch(error => {
-            res.send('error');
-        });
+            .then(() => res.redirect('/'))
+            .catch(error => {
+                res.send('error');
+            });
     }
-     // [GET] /courses/:id/edit
-     edit(req, res, next) {
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
         Course.findById(req.params.id).lean()
-        .then(courses => res.render('courses/edit', {
-            courses: courses
-        }))
-        .catch(next);
+            .then(courses => res.render('courses/edit', {
+                courses: courses
+            }))
+            .catch(next);
+    }
+
+    // [PUT] /courses/:id
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params.id }, req.body).lean()
+            .then(courses => res.redirect('/me/stored/courses'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id }).lean()
+            .then(() => res.redirect('back'))
+            .catch(next);
     }
 }
 
